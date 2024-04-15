cb073352279cba68ac61742f280419aa9cdbe026
ZayatsMaxim <makszay2013@gmail.com>
2024-04-15 16:28:12 +0300

    fixed employee service

--- src/app/shared/services/employees-data.service.ts
+++ src/app/shared/services/employees-data.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { baseUrl, projectUrl } from '../url-consts'
+import { map } from 'rxjs'
+import { Employee } from '../models/employee.model'
 
 @Injectable({
     providedIn: 'root',
@@ -8,6 +11,18 @@ export class EmployeesDataService {
     constructor(private httpClient: HttpClient) {}
 
     getEmployees() {
-        return this.httpClient.get(``)
+        return this.httpClient.get(`${baseUrl}/${projectUrl}`).pipe(
+            map(response => {
+                return response as Employee[]
+            }),
+        )
+    }
+
+    getEmployeeById(id: number) {
+        return this.httpClient.get(`${baseUrl}/${projectUrl}/${id}`).pipe(
+            map(response => {
+                response as Employee
+            }),
+        )
     }
 }
