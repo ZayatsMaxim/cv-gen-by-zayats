daf7d0adfabaa1eb41549951cf8c54cd7532bf4a
ZayatsMaxim <makszay2013@gmail.com>
2024-05-11 13:04:41 +0300

    project list completed

--- src/app/modules/core/sidenav-content/sidenav-content.component.ts
+++ src/app/modules/core/sidenav-content/sidenav-content.component.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLinkActive, RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidenav-content',
   standalone: true,
-  imports: [CommonModule, MatIconModule, RouterModule, RouterLinkActive],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    RouterModule,
+    RouterLinkActive,
+    TranslateModule,
+  ],
   templateUrl: './sidenav-content.component.html',
   styleUrl: './sidenav-content.component.scss',
 })
